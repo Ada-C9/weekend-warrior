7578da5fab2c0a71fc739c0c85f3f5d11f783dcc
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-23 22:25:02 -0800

    completed the word count activity

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,12 @@
+# Completed 2/23/18
 
+# Returns the number of each word in provided_words.
+def words(provided_words)
+  word_count = {}
+  provided_words_as_array = provided_words.strip.split(/\s+/)
+  provided_words_as_array.each do |word|
+    word_count[word] = 0 if !word_count.has_key?(word)
+    word_count[word] += 1
+  end
+  return word_count
+end
