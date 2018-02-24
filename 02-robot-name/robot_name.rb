cde22a2a2413a8aa7457266b30320ddea9594cf6
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-02-23 21:22:01 -0800

    completed the robot name activity

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,25 @@
+# Completed 2/23/18
 
+# This program creates a robot with a randomly generated name
+class Robot
+
+  attr_reader :name
+
+  def initialize
+    @name = ""  # store the robot's name
+    reset
+  end
+
+  # Reset the name of the robot
+  def reset
+    @name = create_name
+  end
+
+  private
+
+  # Returns a randomly generated name with two uppercase letter and three digits
+  def create_name
+    return "#{rand(65..90).chr}#{rand(65..90).chr}#{rand(100..999)}"
+  end
+
+end
