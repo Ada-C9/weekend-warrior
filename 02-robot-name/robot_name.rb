2acba89da6127fa5c256e877f5035b2bdc32d919
SelamawitA <Selamawit.ainalem@gmail.com>
2018-03-04 22:41:46 -0800

    added methods to create a robot name whose first two letters are an upercase character and last three characters are some digit between 0-9.

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,36 @@
+NUMBER_OF_NUMERICAL_CHARACTERS = 3
+NUMBER_OF_ALPHA_CHARCTERS = 2
 
+class Robot
+  attr_reader :name
+
+  def initialize
+    @name = robot_name_generator
+  end
+
+  def numerical_component
+    numbers = ''
+    NUMBER_OF_NUMERICAL_CHARACTERS.times do
+      numbers += rand(0...9).to_s
+    end
+    return numbers
+  end
+
+  def alpha_characters
+    alphabet = ("A".."Z").to_a
+    chars = ''
+    NUMBER_OF_ALPHA_CHARCTERS.times do
+      chars += alphabet[rand(1..alphabet.length-1)]
+    end
+    return chars
+  end
+
+  def robot_name_generator
+    robot_name = alpha_characters + numerical_component
+  end
+
+  def reset
+    @name = robot_name_generator
+  end
+
+end
